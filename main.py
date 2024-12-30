83c07e40bbf2a744e0c8fb9c26194c9f31dbd327
Goti-staniol <wkopinlinejon@gmail.com>
2024-12-30 15:56:22 +0200

    change player

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 from pygame import *
 import pygame
 
-from typing import Tuple
+from typing import Tuple, Optional
 from random import randint, choice
 
 from time import time
 
 init()
-# mixer.init()
+mixer.init()
 
-# mixer.music.load('sounds/space.ogg')
-# mixer.music.play(-1)
+mixer.music.load('sounds/space.ogg')
+mixer.music.play(-1)
+
+shot_sound = mixer.Sound('sounds/fire.ogg')
 
 FPS = 30
 W, H = 500, 700
@@ -23,10 +25,31 @@ bg = transform.scale(
     (W, H)
 )
 
-enimy_img = 'images/ufo.png'
+enemies = []
+asteroid_lst = []
 
-enimy_lst = []
-bullets = []
+
+class Text(sprite.Sprite):
+    def __init__(
+        self,
+        text: str,
+        position: Tuple[int, int],
+        size: int,
+        color: Tuple[int, int, int],
+        font: Optional[str]
+    ):
+        super().__init__()
+        self.position = position
+        self.font = pygame.font.Font(font, size)
+        self.text = self.font.render(text, True, color)
+    
+    def draw(self):
+        window.blit(self.text, self.position)
+
+
+class D(Text):
+    def __init__(self, text, position, size, color, font=None):
+        super().__init__(text, position, size, color, font)
 
 
 class Player(sprite.Sprite):
@@ -34,96 +57,118 @@ class Player(sprite.Sprite):
         self,
         position: Tuple[int, int],
         size: Tuple[int, int],
-        speed,
-        bullet_size: Tuple[int, int],
-        bullet_img: str,
-        img: str
+        bulets_size: Tuple[int, int],
+        speed: int,
+        player_img: str,
+        bullets_img: str
     ):
-        super().__init__()
-        self.img = transform.scale(
-            image.load(
-                img
-            ), 
-            size
+        self.player_img = transform.scale(
+            surface=image.load(player_img), 
+            size=size
         )
-        self.speed = speed
-        self.rect = self.img.get_rect()
+        self.bullets = []
+        self.rect = self.player_img.get_rect()
         self.rect.x, self.rect.y = position
-        self.bullet = transform.scale(
-            image.load(
-                bullet_img
-            ), bullet_size
-        ) 
-        self.spawn_time = time()
-        self.interval = 0.8
+        self.speed = speed
+        self.bullets_speed = 0.8
+        self.bullets_img = transform.scale(
+            surface=image.load(bullets_img), 
+            size=bulets_size
+        )
     
     def move(self, keys) -> None:
-        if keys[K_a]:
-            self.rect.x -= self.speed
         if keys[K_d]:
             self.rect.x += self.speed
+            
+        if keys[K_a]:
+            self.rect.x -= self.speed
         
         if keys[K_LSHIFT]:
-            current_time = time()
-            if current_time - self.spawn_time >= self.interval:
-                bullet_rect = self.bullet.get_rect(center=(self.rect.x + 20, self.rect.y + 35))
-                bullets.append(bullet_rect)
-                
-                self.spawn_time = current_time
-            
+            shot_sound.play(1)
+            self.bullets.append(self.bullets_img.get_rect(
+                center=((self.rect.x + 20, self.rect.y + 35))
+            ))
+    
     def update_bullets(self):
-        for bullet in  bullets:
-            bullet.y -= 5
+        for bullet in self.bullets:
+            for enemy in enemies:
+                if enemy.ufo_rect.colliderect(bullet):
+                    enemies.remove(enemy)
+                    self.bullets.remove(bullet)
+                    
+            if bullet.y <= 0:
+                self.bullets.remove(bullet)
+                
+            bullet.y -= 9
             
-            for enimy in enimy_lst:
-                for bullet in bullets:
-                    if bullet.colliderect(enimy.rect):
-                        enimy_lst.remove(enimy)
-                        bullets.remove(bullet)
-    
-    
+            print(self.bullets)
+            
     def draw(self) -> None:
-        for bullet in bullets:
-            window.blit(self.bullet, bullet)
-        window.blit(self.img, (self.rect.x, self.rect.y))
+        for bullet in self.bullets:
+            window.blit(self.bullets_img, bullet)
+            
+        window.blit(
+            source=self.player_img,
+            dest=(self.rect.x, self.rect.y)
+        )
 
 
-class Enimy(sprite.Sprite):
+class Enemy(sprite.Sprite):
     def __init__(
         self,
-        size: Tuple[int, int],
-        speed,
-        player: 'Player',
-        img: str
+        ufo_size: Tuple[int, int],
+        asteroid_size: Tuple[int, int],
+        speed: int,
+        ufo_img: str,
+        asteroid_img: str,
+        player: 'Player'
     ):
-        super().__init__()
-        self.img = transform.scale(
-            image.load(
-                img
-            ), size
+        self.ufo_img = transform.scale(
+            surface=image.load(ufo_img),
+            size=ufo_size
         )
+        self.ufo_rect = self.ufo_img.get_rect()
+        self.ufo_rect.x = randint(100, 400)  
+        self.ufo_rect.y = randint(80, 100)
+        
+        self.asteroid_img = transform.scale(
+            surface=image.load(asteroid_img),
+            size=asteroid_size
+        )
+        self.asteroid_rect = self.asteroid_img.get_rect()
+        self.asteroid_rect.x = randint(100, 400)  
+        self.asteroid_rect.y = randint(80, 100)
+        
+        self.player_rect = player.rect
+        
         self.speed = speed
-        self.rect = self.img.get_rect()
-        self.rect.x, self.rect.y = randint(200, 400), randint(10, 50)
-        self.player = player
-    
-    def move(self) -> None:
-        self.rect.y += self.speed
     
     def update(self):
-        for enimy in enimy_lst:
-            if enimy.rect.colliderect(self.player.rect):
-                print('Програв')
-            enimy.draw()
-            enimy.move()
+        if self.asteroid_rect.colliderect(self.player_rect) or (
+            self.ufo_rect.colliderect(self.player_rect)
+        ):
+            print('ты проиграл')
+        
+    def move(self):
+        self.asteroid_rect.y += self.speed
+        self.ufo_rect.y += self.speed
     
-    def draw(self) -> None:
-        window.blit(self.img, (self.rect.x, self.rect.y))
+    def draw(self):
+        window.blit(self.ufo_img, (self.ufo_rect.x, self.ufo_rect.y))
+        window.blit(self.asteroid_img, (self.asteroid_rect.x, self.asteroid_rect.y))
+    
+timer = 0
 
-player = Player((250, 550), (50, 50), 5, (20, 20), 'images/bullet.png','images/rocket.png')
+player = Player(
+    position=(250, 500),
+    size=(50, 100),
+    bulets_size=(10, 10),
+    speed=8,
+    player_img='images/rocket.png',
+    bullets_img='images/bullet.png'
+)
 
-spawn_time = time()
-interval = 2
+
 
 game = True
 while game:
@@ -134,16 +179,30 @@ while game:
     player.draw()
     player.move(keys)
     
-    currnet_time = time()
+    timer += 1
+    if timer >= 30:
+        enemies.append(
+            Enemy(
+                ufo_size=(50, 50),
+                asteroid_size=(50, 50),
+                speed=3,
+                ufo_img='images/ufo.png',
+                asteroid_img='images/asteroid.png',
+                player=player
+            )
+        )
+        timer = 0
     
-    if currnet_time - spawn_time >= interval:
-        enimy_lst.append(Enimy((30, 40), 2, player, enimy_img))
-        
-        spawn_time = currnet_time
-    
-    for enimy in enimy_lst:
-        enimy.update()
-    
+    for enemy in enemies:
+        if not enemy.asteroid_rect.colliderect(
+            enemy.ufo_rect
+        ):
+            enemy.update()
+            enemy.draw()
+            enemy.move()
+            
+            
+            
     for e in event.get():
         if e.type == QUIT:
             game = False
